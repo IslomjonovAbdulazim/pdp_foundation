05ffbaae38e29884e223d37b2c7bba0fc9e81d7b
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-20 19:16:41 +0500

    Abduazim Islomjonov: pdp foundation

---
+++ lib/domain/entities/leaderboard/leaderboard_user_entity.dart
@@ -0,0 +1,11 @@
+class LeaderboardUserEntity {
+  final String userID;
+  final int points;
+  final int rank;
+
+  LeaderboardUserEntity({
+    required this.userID,
+    required this.points,
+    required this.rank,
+  });
+}
--- lib/domain/entities/leaderboard/point_entity.dart
+++ lib/domain/entities/leaderboard/point_entity.dart
@@ -1,1 +1,11 @@
+class PointEntity {
+  final int point;
+  final DateTime time;
+  final String userID;
 
+  PointEntity({
+    required this.point,
+    required this.time,
+    required this.userID,
+  });
+}
