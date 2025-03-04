f56f37fc53652bd66b6843b5236fe452cfe8dc79
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-04 13:29:13 +0500

    Abduazim Islomjonov: pdp foundation

--- lib/app/features/study/quiz/imports.dart
+++ lib/app/features/study/quiz/imports.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pdp_foundation/app/controllers/study/quiz_controller.dart';
+import 'package:pdp_foundation/utils/extenstions/text_style_extention.dart';
 
 part 'quiz_page.dart';
 part 'quiz_animations.dart';
--- lib/app/features/study/quiz/quiz_page.dart
+++ lib/app/features/study/quiz/quiz_page.dart
@@ -1,10 +1,27 @@
 part of 'imports.dart';
 
-class QuizPage extends StatelessWidget {
+class QuizPage extends GetView<QuizController> {
   const QuizPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Obx(
+          () => controller.isLoading.value
+              ? const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                )
+              : Column(
+                  children: [
+                    Text(
+                      controller.quiz.value.questions.length.toString(),
+                      style: context.title,
+                    ),
+                  ],
+                ),
+        ),
+      ),
+    );
   }
 }
