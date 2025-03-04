418e7277a24d9688d27438a59f6bbe01cb8927a8
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-04 13:26:27 +0500

    Abduazim Islomjonov: pdp foundation

--- lib/app/controllers/study/quiz_controller.dart
+++ lib/app/controllers/study/quiz_controller.dart
@@ -1,4 +1,19 @@
 import 'package:get/get.dart';
+import 'package:pdp_foundation/app/data/models/quiz/quiz_model.dart';
 
-class QuizController extends GetxController {}
+class QuizController extends GetxController {
+  RxBool isLoading = false.obs;
+  Rx<QuizModel> quiz = QuizModel(id: -1, questions: []).obs;
 
+  @override
+  void onInit() {
+    init();
+    super.onInit();
+  }
+
+  void init() async {
+    isLoading.value = true;
+    quiz.value = mockQuiz;
+    isLoading.value = false;
+  }
+}
