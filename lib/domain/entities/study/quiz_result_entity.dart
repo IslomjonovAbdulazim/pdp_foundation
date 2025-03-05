a206dabbb45577628e3180198767a8fffabb2a28
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-05 13:39:25 +0500

    Abduazim Islomjonov: pdp foundation

--- lib/domain/entities/study/quiz_result_entity.dart
+++ lib/domain/entities/study/quiz_result_entity.dart
@@ -5,6 +5,7 @@ class QuizResultEntity {
   final List<int> wrongs;
   final DateTime start;
   final DateTime end;
+  final String topic;
 
   QuizResultEntity({
     required this.quizID,
@@ -13,5 +14,6 @@ class QuizResultEntity {
     required this.wrongs,
     required this.start,
     required this.end,
+    required this.topic,
   });
 }
